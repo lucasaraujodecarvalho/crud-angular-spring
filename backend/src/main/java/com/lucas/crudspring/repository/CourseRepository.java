d1ec272552628565d8e3323af240ca36389fcd2e
Lucas <lucas.car.araujo@gmail.com>
2023-01-16 22:12:09 -0300

    adicionando controller, repository e criação do model

---
+++ backend/src/main/java/com/lucas/crudspring/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.crudspring.repository;
+
+import com.lucas.crudspring.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
